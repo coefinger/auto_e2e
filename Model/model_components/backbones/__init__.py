09683eb04fef42224553d3b79d8d25d3ac0ced5e
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2026-06-05 07:48:40 +0300

    creating backbone registry

---
+++ Model/model_components/backbones/__init__.py
@@ -0,0 +1,16 @@
+from .conv_next_v2_tiny import ConvNextV2Tiny
+from .swin_v2_tiny import SwinV2Tiny
+
+BACKBONE_REGISTRY = {
+    "swin_tiny": ConvNextV2Tiny,
+    "swin_v2_tiny": SwinV2Tiny,
+}
+
+
+def build_backbone(backbone):
+    if backbone not in BACKBONE_REGISTRY:
+        raise ValueError(
+            f"Unknown backbone '{backbone}'. "
+            f"Available: {list(BACKBONE_REGISTRY.keys())}"
+        )
+    return BACKBONE_REGISTRY[backbone]
