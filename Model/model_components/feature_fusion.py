1c35f04b3abc46c290986076e7b9c42facdba5e0
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2026-06-05 08:51:01 +0300

    accounting for ordering of features in SwinV2 vs ConvNextV2 and permuting only SwinV2 features for downstream modules

--- Model/model_components/feature_fusion.py
+++ Model/model_components/feature_fusion.py
@@ -24,19 +24,35 @@ class FeatureFusion(nn.Module):
         # View fusion strategy (pluggable)
         self.view_fusion = build_view_fusion(fusion_mode, num_views, embed_dim)
 
-    def forward(self, features, B, V, camera_params=None):
+    def forward(self, features, B, V, backbone="swin_v2_tiny", camera_params=None):
         # features: list of 4 multi-scale feature maps from backbone
         # Each has shape [B*V, H, W, C] (SwinV2 output format)
 
-        f0 = self.pool(features[0].permute(0, 3, 1, 2))
-        f1 = self.pool(features[1].permute(0, 3, 1, 2))
-        f2 = self.pool(features[2].permute(0, 3, 1, 2))
-        f3 = features[3].permute(0, 3, 1, 2)
+        f0 = self.pool(features[0])
+        f1 = self.pool(features[1])
+        f2 = self.pool(features[2])
+        f3 = features[3]
 
-        # Concatenate scales along channels: [B*V, 1440, 7, 7]
+        # Permuting features to B*V, C, H, W for SwinV2
+        # with pooling
+        if(backbone == "swin_v2_tiny"):
+            f0 = self.pool(features[0].permute(0, 3, 1, 2))
+            f1 = self.pool(features[1].permute(0, 3, 1, 2))
+            f2 = self.pool(features[2].permute(0, 3, 1, 2))
+            f3 = features[3].permute(0, 3, 1, 2)
+        
+        # Pooling features from ConvNextV2 without permutation
+        if(backbone == "conv_next_v2_tiny"):
+            f0 = self.pool(features[0])
+            f1 = self.pool(features[1])
+            f2 = self.pool(features[2])
+            f3 = features[3]
+
+
+        # Concatenate scales along channels: [B*V, 1440, 8, 8]
         fused_per_view = torch.cat((f0, f1, f2, f3), dim=1)
 
-        # Unify across views: [B*V, 1440, 7, 7] → [B, 1440, 7, 7]
+        # Unify across views: [B*V, 1440, 8, 8] → [B, 1440, 8, 8]
         # camera_params is passed through for BEV fusion; ignored by other modes
         fused = self.view_fusion(fused_per_view, B, V, camera_params=camera_params)
 
